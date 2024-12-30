be8e27cafa6db512e7a8ad72c7ec357e62517489
Obua Stella <stellaobua123@gmail.com>
2024-12-30 22:40:22 +0100

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div className="text-center text-2xl my-10">
+      Attendence Management Software <br />
+      <Button>Button</Button>
+    </div>
+  );
+}
