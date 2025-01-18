1d39a6512304db77ef35752e47c5ceccf9b38b74
Zion <omojolazion@gmail.com>
2025-01-18 04:20:48 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Auth/Student/Login";
 import SignUp from "./pages/Auth/Student/SignUp";
-import LecturerLogin from "./pages/Auth/Lecturer/LecturerLogin";
-import LecturerSignup from "./pages/Auth/Lecturer/LecturerSignup";
+// import LecturerLogin from "./pages/Auth/Lecturer/LecturerLogin";
+// import LecturerSignup from "./pages/Auth/Lecturer/LecturerSignup";
 
 export default function App() {
   return (
@@ -10,8 +10,10 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/lecturer/login" element={<LecturerLogin />} />
-        <Route path="/lecturer/signup" element={<LecturerSignup />} />
+        {/* Remove the comments in the codes, probably work at your end */}
+        
+        {/* <Route path="/lecturer/login" element={<LecturerLogin />} /> */}
+        {/* <Route path="/lecturer/signup" element={<LecturerSignup />} /> */}
       </Routes>
     </Router>
   );
