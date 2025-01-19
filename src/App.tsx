c0c304bf23a47aa95f44092b0c5d659dab508750
Zion <omojolazion@gmail.com>
2025-01-19 19:25:00 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Auth/Student/Login";
 import SignUp from "./pages/Auth/Student/SignUp";
 import LecturerLogin from "./pages/Auth/Lecturer/LecturerLogin";
-import LecturerSignup from "./pages/Auth/Lecturer/LecturerSignUp";
+import LecturerSignup from "./pages/Auth/Lecturer/LecturerSignup";
 
 export default function App() {
   return (
